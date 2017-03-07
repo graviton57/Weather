99407d4e361a3aed1563a1dfb312922a26650e8f
graviton57 <gavrilyuk.igor@gmail.com>
2017-03-07 20:05:51 +0200

    add versions.gradle

--- app/src/main/java/com/havrylyuk/weather/widget/TodayWidgetIntentService.java
+++ app/src/main/java/com/havrylyuk/weather/widget/TodayWidgetIntentService.java
@@ -1,6 +1,5 @@
 package com.havrylyuk.weather.widget;
 
-import android.annotation.TargetApi;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -8,7 +7,6 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
@@ -31,7 +29,6 @@ import com.havrylyuk.weather.activity.CitiesActivity;
 import com.havrylyuk.weather.dao.OrmCity;
 import com.havrylyuk.weather.dao.OrmWeather;
 import com.havrylyuk.weather.data.local.ILocalDataSource;
-import com.havrylyuk.weather.data.local.LocalDataSource;
 import com.havrylyuk.weather.service.WeatherService;
 import com.havrylyuk.weather.util.PreferencesHelper;
 
@@ -148,7 +145,6 @@ public class TodayWidgetIntentService extends IntentService {
         }, CallerThreadExecutor.getInstance());
     }
 
-    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
     private void setRemoteContentDescription(RemoteViews views, String description) {
         views.setContentDescription(R.id.widget_icon, description);
     }
